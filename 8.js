f1af04898e8535595248b5c72edccbe62a712b96
Sharada24git <sharushambhavi24@gmail.com>
2025-08-30 17:42:01 +0530

    Add files via upload

---
+++ 8.js
@@ -0,0 +1,9 @@
+function checkIfAdult(){
+    let number = 3;
+    if(number<18){
+        console.log("This person is not Adult");
+    }else{
+        console.log("This person is Adult")
+    }
+}
+checkIfAdult();
